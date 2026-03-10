87fc2675f5c18c4e1de6275c6d565f3760955114
Ricky <evholtz@gmail.com>
2026-03-10 16:14:50 +0000

    Add fizzbuzz.py implementing classic FizzBuzz from 1 to 100 (1/1)

---
+++ fizzbuzz.py
@@ -0,0 +1,15 @@
+"""Simple FizzBuzz implementation printing numbers from 1 to 100."""
+
+def fizzbuzz():
+    for i in range(1, 101):
+        if i % 15 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
+if __name__ == "__main__":
+    fizzbuzz()

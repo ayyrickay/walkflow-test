be523abd60e61a2fc2fafd94763d47afe4897d18
Ricky <evholtz@gmail.com>
2026-03-10 04:35:50 +0000

    Add example implementation and tests for 'this buzz' (1/2)

---
+++ this_buzz_example.py
@@ -0,0 +1,7 @@
+"""Example implementation demonstrating 'this buzz'."""
+
+def buzz(value):
+    """Simple function that returns 'Buzz!' if value is divisible by 3, else returns the value."""
+    if value % 3 == 0:
+        return 'Buzz!'
+    return value
